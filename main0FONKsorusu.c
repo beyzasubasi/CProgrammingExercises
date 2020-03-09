4d55d2e89337248b8e86ed7361faaa3f9b295dfa
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2020-03-09 21:41:09 +0300

    Create main0FONKsorusu.c

---
+++ main0FONKsorusu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* klavyeden 2sayýyý toplayýp sonucu ekrana yazdýran (fonk ile) C kodu */
+
+int main() {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
