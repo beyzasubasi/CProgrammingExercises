74a527cd2247211d6f25f9302b04aeeb5525e746
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2020-03-09 21:20:57 +0300

    Create main7dikdöralançevre.c

---
+++ main7dikdöralançevre.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+
+//dikdörtgen alan ve çevresi
+//deðer: kýsa kenar, uzun kenar
+//alan: kýsakenar*uzunkenar
+//çevre: kýsakenar+kýsakenar+uzunkenar+uzunkenar
+
+
+
+{
+	int kisa,uzun,kenar,alan,cevre;
+
+kisa=8;
+uzun=12;
+alan=kisa*uzun;
+cevre=2*(kisa+uzun);
+
+printf("dikdortgenin alani: %d\n",alan);
+printf("dikdortgenin cevresi: %d",cevre);
+
+	
+	
+	
+	
+	return 0;
+}
