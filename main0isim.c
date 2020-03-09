30d003ad651807a47aa377c70a670160218f5538
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2020-03-09 21:40:57 +0300

    Create main0isim.c

---
+++ main0isim.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	printf("Ad: Baran\n");
+	printf("Soyad: Yucedag\n");
+    printf("Maas: 2500\n");
+	printf("Semt: Seyhan");
+	
+	return 0;
+}
